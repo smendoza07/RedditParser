a2b2e0748617674d7277a0e664917cb4aab1c5f6
Saul <mendozasaul07@gmail.com>
2023-05-03 21:15:12 -0700

    Added loop to collect random subreddits and links from body

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import praw
 import json
 import os
+import requests
+from bs4 import BeautifulSoup
+import html
+import prawcore
 
 # Reddit API credentials
 reddit = praw.Reddit(client_id='GUXF6LGTVNbixiD8PPns2Q',
@@ -10,111 +14,93 @@ reddit = praw.Reddit(client_id='GUXF6LGTVNbixiD8PPns2Q',
                     password='aPQZWqL&-)vxi)9')
 
 # Maximum size of the JSON file in bytes
-max_file_size = 10000 * 1024  # 1 KB
+max_file_size = 100 * 1024  # 1 KB
 
 # Maximum size of all JSON files combined in bytes
-max_total_size = 100000 * 1024 # 1 MB
+max_total_size = 1000 * 1024 # 1 MB
 
-
-# User input of the subreddit to parse
-# subreddit_name = input('Enter the name of the subreddit you would like to crawl: ')
-subreddit_name = 'python'
-
-# Get the top x posts from the subreddit
-posts = reddit.subreddit(subreddit_name).top(limit=None)
+# Create data folder if it doesn't exist
+if not os.path.exists('data'):
+    os.mkdir('data')
 
 # List to store post and comment data
 data = []
 
 # Set to keep track of post and comment IDs
 post_ids = set()
-comment_ids = set()
 
-# Counter for file number
-file_num = 1
+hyperlinks = []
+link_title = ''
 
-# Create data folder if it doesn't exist
-# if not os.path.exists('data'):
-#     os.mkdir('data')
+while True:
 
-# Loop through each post and its comments
-for post in posts:
+    # Counter for file number
+    file_num = 1
 
-    # Skip duplicate posts
-    if post.id in post_ids:
-        continue
+    # Get a random subreddit
+    subreddit_name = reddit.random_subreddit(nsfw=False).display_name
 
-    post_data = {
-        'title': post.title,
-        'url': post.url,
-        'author': post.author.name if post.author else None,
-        'score': post.score,
-        'body': post.selftext,
-        'created_utc': post.created_utc,
-        'comments': []
-    }
+    # Get the top x posts from the subreddit
+    posts = reddit.subreddit(subreddit_name).top(limit=None)
 
-    post.comments.replace_more(limit=None)
+    # Loop through each post and its comments
+    for post in posts:
 
-    for comment in post.comments:
-        # Skip duplicate comments
-        if comment.id in comment_ids:
+        html_body = post.selftext_html
+
+        # Decode the HTML and extract all hyperlinks
+        if html_body:
+            text_body = html.unescape(html_body)
+            hyperlinks = [link['href'] for link in BeautifulSoup(text_body, 'html.parser').find_all('a')]
+            
+            # For each hyperlink, send a GET request to the URL and extract the page title
+            for link in hyperlinks:
+                try:
+                    response = prawcore.requests.get(link, timeout=5,)
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    link_title = soup.title.string.strip()
+                except:
+                    link_title = [f"Could not get title"]
+        else:
+            hyperlinks = []
+            link_title = ''
+
+        if post.id in post_ids:
             continue
 
-        if isinstance(comment, praw.models.MoreComments):
-            continue
-
-        comment_data = {
-            'body': comment.body,
-            'author': comment.author.name if comment.author else None,
-            'score': comment.score,
-            'created_utc': comment.created_utc,
-            'replies': []
+        post_data = {
+            'title': post.title,
+            'post_url': f"https://www.reddit.com{post.permalink}",
+            'author': post.author.name if post.author else None,
+            'url': post.url,
+            'links': { 'title': link_title,
+                    'link': [link for link in hyperlinks],
+                    },
         }
 
-        comment.replies.replace_more(limit=None)
+        data.append(post_data)
 
-        # Loop through each reply to the comment
-        for reply in comment.replies:
-            if isinstance(reply, praw.models.MoreComments):
-                continue
+        # Add post ID to set of seen post IDs
+        post_ids.add(post.id)
 
-            reply_data = {
-                'body': reply.body,
-                'author': reply.author.name if reply.author else None,
-                'score': reply.score,
-                'created_utc': reply.created_utc
-            }
+        # Write the data to a JSON file and check the file size and total size
+        file_name = f'data/reddit_data_{subreddit_name}_{file_num}.json'
+        with open(file_name, 'w') as f:
+            json.dump(data, f, indent=4)
 
-            comment_data['replies'].append(reply_data)
+        data_folder = 'data'
+        file_size = os.path.getsize(file_name)
+        total_size = sum(os.path.getsize(os.path.join(data_folder, f)) for f in os.listdir(data_folder))
 
-        post_data['comments'].append(comment_data)
+        if file_size >= max_file_size:
+            # Start a new file if the current file size is too large
+            file_num += 1
+            data = []
+        if total_size >= max_total_size:
+            # Stop crawling if the total size of all files is too large
+            break
 
-        # Add comment ID to set of seen comment IDs
-        comment_ids.add(comment.id)
-
-    data.append(post_data)
-
-    # Add post ID to set of seen post IDs
-    post_ids.add(post.id)
-
-    # Write the data to a JSON file and check the file size and total size
-    file_name = f'reddit_data_{file_num}.json'
-    with open(file_name, 'w') as f:
-        json.dump(data, f, indent=4)
-
-    file_size = os.path.getsize(file_name)
-    total_size = sum(os.path.getsize(f) for f in os.listdir() if f.startswith('reddit_data_'))
-
-    print(f'File name: {file_name}')
-    print(f'File size: %.2f MB / %.2f MB' % (file_size/1024/1000, max_file_size/1024/1000))
-    print(f'Total size: %.2f MB / %.2f MB' % (total_size/1024/1000, max_total_size/1024/1000))
-    print(20*'-')
-
-    if file_size >= max_file_size:
-        # Start a new file if the current file size is too large
-        file_num += 1
-        data = []
-    elif total_size >= max_total_size:
-        # Stop crawling if the total size of all files is too large
+    # Check if total size limit is reached
+    total_size = sum(os.path.getsize(os.path.join(data_folder, f)) for f in os.listdir(data_folder))
+    if total_size >= max_total_size:
         break
